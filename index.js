e4338493643f83958190e40428b9db57a0635aa1
Ksinia <xiatian@mail.ru>
2019-11-15 22:09:34 +0100

    Initial version of keyword buttons

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 function getRandomAdvice() {
     fetch("https://api.adviceslip.com/advice") // make the request
         .then(response => response.json()) // convert response to json
-        .then(myJson => { // display data in the browser
+        .then(async myJson => { // display data in the browser
             const adviceText = myJson.slip.advice
             document.getElementById("advice").innerHTML = adviceText
-            // keywords(adviceText)
+            const filtered = await keywords(adviceText)
+            const keywordsDiv = document.getElementById("keywords")
+            filtered.forEach(word => {
+                const btn = document.createElement("button")
+                btn.innerHTML = word
+                btn.onclick = async function () {
+                    myJson = await searchK(word)
+                    const qty = myJson.slips.length
+                    const rand = Math.floor(Math.random() * qty)
+                    document.getElementById('advice').innerHTML = myJson.slips[rand].advice
+                }
+                keywordsDiv.appendChild(btn)
+            })
         })
 }
 
-// function searchAdvice() {
-//     const keyword = document.getElementById('keyword').value
-//     console.log(keyword)
-//     fetch(`https://api.adviceslip.com/advice/search/${keyword}`) // make the request
-//         .then(response => response.json()) // convert response to json
-//         .then(myJson => { // display data in the browser
-//             const qty = myJson.slips.length
-//             console.log(qty)
-//             const rand = Math.floor(Math.random() * qty)
-//             console.log(rand)
-//             document.getElementById("advice").innerHTML =
-//                 myJson.slips[rand].advice
-//             // "<pre>" + JSON.stringify(myJson, null, " ") + "</pre>"
-//         })
-// }
 async function searchAdvice1() {
     const keyword = document.getElementById('keyword').value
     // console.log(keyword)
@@ -34,28 +31,34 @@ async function searchAdvice1() {
     // console.log(rand)
     document.getElementById("advice").innerHTML =
         myJson.slips[rand].advice
-
-
 }
 
-function searchK(keyword) {
-    return fetch(`https://api.adviceslip.com/advice/search/${keyword}`) // make the request
-        .then(response => response.json()) // convert response to json
+async function searchK(keyword) {
+    const response = await fetch(`https://api.adviceslip.com/advice/search/${keyword}`)
+    let data = await response.json()
+    return data
 }
 
 async function keywords(sentence) {
     const arr = sentence.split(' ')
-    const filteredArr = arr.filter(async (element) => {
-        console.log("Element: ", element)
+    const filteredArr = await filter(arr, async element => {
+        if (element.length < 5) {
+            return false
+        }
+        // console.log("Element: ", element)
         const searchRes = await searchK(element)
         const searchResQty = searchRes.total_results
-        console.log("Search Result:", searchRes)
-        console.log("Search Result Qty:", searchResQty)
-        console.log("Search Result Qty > 1", searchRes > 1)
+        // console.log("Search Result:", searchRes)
+        // console.log("Search Result Qty:", searchResQty)
+        // console.log("Search Result Qty > 1 is", searchResQty > 1)
         return searchResQty > 1
     })
-    console.log("Filtered array", filteredArr)
     return filteredArr
+}
+
+async function filter(arr, callback) {
+    const fail = Symbol()
+    return (await Promise.all(arr.map(async item => (await callback(item)) ? item : fail))).filter(i => i !== fail)
 }
 
 getRandomAdvice()
